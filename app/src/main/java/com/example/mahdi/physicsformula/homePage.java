cd8e8651a9c9e1dfb54f0df3b14f4d29b3c9a4f3
mahdi_mef <mahdieshghi201616@gmail.com>
2018-12-27 13:43:44 +0330

    add btn back (vector)

--- app/src/main/java/com/example/mahdi/physicsformula/homePage.java
+++ app/src/main/java/com/example/mahdi/physicsformula/homePage.java
@@ -1,22 +1,29 @@
 package com.example.mahdi.physicsformula;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
 public class homePage extends AppCompatActivity {
-
+Button btnclass12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        btnclass12=findViewById(R.id.btn_book12);
 
 
     }
-
+    public void btnClass12(View view) {
+        Intent intent= new Intent(homePage.this,ListPage.class);
+        startActivity(intent);
+    }
 
 }
